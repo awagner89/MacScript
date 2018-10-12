fc2a2667eddb5ff2db05afd141cbbcd0762c8972
Neil Johnson <neil.johnson@thetradedesk.com>
2018-10-12 09:32:38 -0700

    Added chmod to shared file to make sure it is executable the second time

--- MacScript.sh
+++ MacScript.sh
@@ -123,6 +123,8 @@ elif [ "$1" == "-ADJoin" ]; then
 		  sudo -k
 		fi
 	done
+
+	sudo chmod a+x /User/Shared/"${my_name}"
 	
 	# Set the computer to not sleep
 	#echo "Setting the computer to never sleep as to not affect the downloads"
